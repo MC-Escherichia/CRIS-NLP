ff6377bafb87ea31de25b52526f599c6301438b0
Matthew Conway <matthew.f.conway@gmail.com>
2014-11-10 18:20:34 -0500

    * common/analysis/trend_utils.jl: look for trens across all years

--- common/analysis/trend_utils.jl
+++ common/analysis/trend_utils.jl
@@ -10,11 +10,14 @@ function detect_trends(min_end_count::Int, min_mult::Float64)
     (term, history) -> detect_trends(term, history, min_end_count, min_mult)
 end
 
+function well_ordered(arr::Float64[])
+    sort(arr)==arr
+end
 # schema of history is {(year: Int, term: ASCIIString, count: Int, freq: Float64)}
 function detect_trends(term::ASCIIString, history::Vector{Tuple},
                        min_end_count::Int, min_mult::Float64)
     sort!(history, by=(t->t[1]))
-    
+
     first_year = history[1][1]
     last_year = history[end][1]
     span = last_year - first_year + 1
@@ -38,18 +41,21 @@ function detect_trends(term::ASCIIString, history::Vector{Tuple},
         arr_year += 1
     end
 
+
     trends = Tuple[]
     for i = 3:length(freqs)
-        if (counts[i] >= min_end_count &&
-            freqs[i] / freqs[i-2] >= min_mult &&
-            (freqs[i-2] <= freqs[i-1] <= freqs[i]))
+        for j = 1:i-1
+            if (counts[i] >= min_end_count &&
+                freqs[i] / freqs[i-j] >= min_mult &&
+                 well_ordered(freqs[i-j:i]))
 
-            start_year = (first_year-1) + (i-2)
+            start_year = (first_year-1) + (i-j)
             end_year = (first_year-1) + i
-            push!(trends, (start_year, end_year, term, counts[i], freqs[i], freqs[i] / freqs[i-2]))
+            push!(trends, (j , start_year, end_year, term, counts[i], freqs[i], freqs[i] / freqs[i-j]))
+            end
         end
     end
-    
+
     trends
 end
 
